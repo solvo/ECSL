0dba37d06eccd0bd478c3d6340646158a011474e
Maikel <maikel8907@nauta.cu>
2016-12-08 11:50:58 -0600

    Initializing the project

--- system/models.py
+++ system/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django.db.models import *
 from django.utils.translation import ugettext_lazy as _
 
-# Faltan Question y Question Category1
+# Faltan Question y Question Category
 
 
 class Profile(Model):
