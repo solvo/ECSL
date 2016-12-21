7ef163d3e8c2174128baf8dd95c8f5c955b2a71a
Omar <chicomtz.sr@gmail.com>
2016-12-22 00:20:09 +0100

    Updating

--- system/admin.py
+++ system/admin.py
@@ -1,3 +1,5 @@
+from _csv import list_dialects
+
 from django.contrib import admin
 from system.models import *
 # Register your models here.
@@ -8,4 +10,11 @@ admin.site.register(Inscription)
 admin.site.register(SpeechType)
 admin.site.register(Topic)
 admin.site.register(DateState)
-admin.site.register(Speech)
+
+
+@admin.register(Speech)
+class SpeechAdmin(admin.ModelAdmin):
+    list_display = ('topic', 'user', 'audience', 'published')
+    #list_display_links = ('published',)
+    list_editable = ('published',)
+
